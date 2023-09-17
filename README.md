2b61edb6da4bc1c40a0e0cf48baf5eb9f20ec586
Israel <israel.carvalho@sou.inteli.edu.br>
2023-09-17 04:44:11 -0300

    feat. teste automatizado + docs

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # Testes Automatizados
+
+## Objetivo
+Verificar se um usuário pode criar uma nova iniciativa na aplicação.
+
+## Pré-condição
+As iniciativas devem estar listadas e visíveis na tela respectiva.
+
+## Procedimento de Teste Manual
+
+1. Verificar se as iniciativas estão listadas na tela.
+2. Clicar no botão "Criar +".
+3. Preencher os campos obrigatórios, como Título, Empresa, Módulo e Status, com os seguintes valores:
+   - Título: "Criação de Website de Vendas"
+   - Empresa: "MRV"
+   - Módulo: "Projeto de Arquitetura de Software - Engenharia de Software"
+   - Status: "Pendente"
+4. Clicar no botão "Criar".
+
+
+# Resultado do Teste Manual
+
+**Resultado Esperado**
+A iniciativa deve ser criada com sucesso, e o usuário deve ser redirecionado para a página de listagem de iniciativas, onde a nova iniciativa deve ser exibida ao final da página.
+
+**Resultado Obtido**
+[Descreva aqui o resultado obtido após a execução do teste automatizado. Por exemplo, se o teste passou ou falhou, e quaisquer observações relevantes.]
+
+**Pós-condição**
+O sistema agiu como o esperado, não sendo necessário nenhuma operação adicional.
+
+## Procedimento de Teste Automatizado
+
+1. Opcional - Verificar se as iniciativas estão listadas na tela (frontend).
+2. Alterar os dados do dicionário "criar_iniciativa", no arquivo [test.py](code/code.py), para os valores desejados para o teste.
+Ex.:
+    - "name": "Criação de Website de Vendas",
+    - "status": "PENDING",
+    - "description": "Este website será criado para aumentar as vendas da empresa.",
+    - "observation": "Utilizar FastAPI para criar a API do website.",
+    - "company_id": 42,
+    - "module_id": 28
+4. Salvar as alterações, e executar o script pelo terminal com o comando `python3 test.py`. 
+
+# Resultado do Teste Automatizado
+
+**Resultado Esperado:** O console deve apresentar o id da nova tarefa criada, e este deve estar contido na lista de ids das iniciativas. Desta forma, o teste será concluído com sucesso.
+
+**Resultado Obtido:** O teste passou com sucesso. A nova tarefa foi criada com a seguinte menságem: "Teste Passou: O ID da nova iniciativa aparece na lista de iniciativas! Teste finalizado com sucesso!".
+
+**Pós-Condição**
+O sistema agiu como o esperado, não sendo necessário nenhuma operação adicional.
+
+# Imagens
+
+![Tela de criação de iniciativas](./media/criariniciativa.png "Tela de criação de iniciativas")
+
+![Iniciativa criada](./media/criada.png "Iniciativa criada")
+
+![Teste automatizado finalizado com sucesso](./media/automatizado.png "Teste automatizado finalizado com sucesso")
--- code/test.py
+++ code/test.py
@@ -1,25 +1,52 @@
 import requests
+import json
 
 # Verificar se as iniciativas estão acessíveis
-url = "localhost:3000/initiatives"
-response = requests.get(url)
+url_listagem_iniciativas = "http://localhost:3000/initiatives"
+response_listagem = requests.get(url_listagem_iniciativas)
 
-if response.status_code == 200:
+if response_listagem.status_code == 200:
     # Iniciativas acessíveis, criar uma nova iniciativa
-    url_criar_iniciativa = "localhost:3000/initiatives"
+    url_criar_iniciativa = "http://localhost:3000/initiatives"
     criar_iniciativa = {
-    "name": "testeIsrael",
-    "status": "PENDING",
-    "description": "testeIsrael",
-    "observation": "testeIsrael",
-    "company_id": 42,
-    "module_id": 28
+        "name": "Criação de Website de Vendas",
+        "status": "PENDING",
+        "description": "Este website será criado para aumentar as vendas da empresa.",
+        "observation": "Utilizar FastAPI para criar a API do website.",
+        "company_id": 42,
+        "module_id": 28
     }
-    response = requests.post(url_criar_iniciativa, data=criar_iniciativa)
+
+    # Serializar o dicionário como JSON
+    dados_json = json.dumps(criar_iniciativa)
+
+    # Enviar como dados usando o parâmetro data
+    response_criacao = requests.post(url_criar_iniciativa, data=dados_json, headers={'Content-Type': 'application/json'})
 
     # Verificar se a iniciativa foi criada com sucesso
-    if response.status_code == 200:
+    if response_criacao.status_code == 200:
         print("Teste Passou: Iniciativa criada com sucesso!")
+
+        # Analisar a resposta JSON da criação para obter o ID da iniciativa criada
+        nova_iniciativa = response_criacao.json()
+        nova_iniciativa_id = int(nova_iniciativa.get("id"))
+        print("Id da nova iniciativa:", nova_iniciativa_id)
+
+        # Verificar se o ID da nova iniciativa está na lista de iniciativas
+        url_listagem_iniciativas = "http://localhost:3000/initiatives"
+        response_listagem = requests.get(url_listagem_iniciativas)
+
+        if response_listagem.status_code == 200:
+            print("Teste Passou: Iniciativas acessíveis após a nova inserção. Listando Ids das iniciativas:")
+            lista_iniciativas = response_listagem.json().get("data")
+            print([iniciativa["id"] for iniciativa in lista_iniciativas])
+
+            if nova_iniciativa_id in [iniciativa["id"] for iniciativa in lista_iniciativas]:
+                print("Teste Passou: O ID da nova iniciativa aparece na lista de iniciativas! Teste finalizado com sucesso!")
+            else:
+                print("Teste Falhou: O ID da nova iniciativa não aparece na lista de iniciativas.")
+        else:
+            print("Teste Falhou: Iniciativas indisponíveis após a nova inserção.")
     else:
         print("Teste Falhou: Falha ao criar a iniciativa.")
 else:
